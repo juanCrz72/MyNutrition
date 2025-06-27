c517db531c7da2613812545caf6c27bd1e358b52
Marimar94 <166160654+Marimar94@users.noreply.github.com>
2025-06-26 19:23:53 -0600

    Update connection.js

--- Server/db/connection.js
+++ Server/db/connection.js
@@ -4,5 +4,6 @@ export const db = await mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
     password: 'erenjeager', //Contraseña de la base de datos
-    database: 'mynutrit_nutricion' //Nombre de la base de datos
+    database: 'mynutrit_nutricion', //Nombre de la base de datos
+    family: 4
 });
