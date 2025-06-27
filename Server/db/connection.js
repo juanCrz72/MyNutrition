2e9f32498e6aa9c5ed31b080797e48607b60285f
Marimar94 <166160654+Marimar94@users.noreply.github.com>
2025-06-26 19:30:02 -0600

    Update connection.js

--- Server/db/connection.js
+++ Server/db/connection.js
@@ -1,9 +1,8 @@
 import mysql from 'mysql2/promise';
 
 export const db = await mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     user: 'root',
-    password: 'erenjeager', //Contraseña de la base de datos
-    database: 'mynutrit_nutricion', //Nombre de la base de datos
-    family: 4
+    password: '1234', //Contraseña de la base de datos
+    database: 'mynutrit_nutricion' //Nombre de la base de datos
 });
