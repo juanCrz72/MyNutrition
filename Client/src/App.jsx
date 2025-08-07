1a98b605bcf3fea106b6b41c1416b049245f429e
Marimar94 <166160654+Marimar94@users.noreply.github.com>
2025-08-07 13:26:49 -0600

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { AuthProvider, useAuth } from './pages/Superadmin/AuthContext.jsx';
